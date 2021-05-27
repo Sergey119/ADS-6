5490860a73882463d50ee148c4c53d8e6596e82c
Sergey119 <78952666+Sergey119@users.noreply.github.com>
2021-05-27 23:47:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,26 @@
-// Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string>Tree;
+  char difFromAToa = 'a' - 'A';
+  std::string word = "";
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return Tree;
+  }
+  while (!file.eof()) {
+    char ch = file.get();
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+      if (ch >= 'A' && ch <= 'Z')
+        ch += difFromAToa;
+      word += ch;
+    } else if (word != "") {
+      Tree.add(word);
+      word = "";
+    }
+  }
+  file.close();
+  return Tree;
 }
